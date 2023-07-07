54c78113a6a5ae606eb81b272f82f9a60e99cc9d
SaveliiNeielov02 <neelovsavelij@gmail.com>
2023-07-07 19:13:30 +0300

    'Finally'

---
+++ SIOP/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace SIOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Font = new System.Drawing.Font("Toledo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 612);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(834, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(852, 610);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Toledo Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(975, 552);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 586);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Напиши свой запрос модели сюда:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1001, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "Form1";
+            this.Text = "ChatModel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SIOP/Form1.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace SIOP
+{
+    public partial class Form1 : Form
+    {
+        string apiKey = null;
+        string apiUrl = "https://api.openai.com/v1/chat/completions";
+        string systemMessages = "";
+        bool isOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+        public async void Looping() 
+        {
+            while (!isOver)
+            {
+                await Task.Delay(600);
+                if (!isOver)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        richTextBox1.AppendText("*");
+                    }));
+                }
+            }
+        }
+        public string PreprocessInput(string userInput)
+        {
+            userInput = userInput.ToLower();
+            if ((userInput.Contains("посоветуй") && userInput.Contains("книг"))
+                || (userInput.Contains("помо") && userInput.Contains("книг"))
+                || (userInput.Contains("скажи") && userInput.Contains("книг"))
+                || (userInput.Contains("рекомен") && userInput.Contains("книг")))
+            {
+                if (userInput.Contains("жанр"))
+                {
+                    return "Жанр указан";
+                }
+                else
+                {
+                    return "Не указан жанр";
+                }
+            }
+            else 
+            {
+                return "Не книжная тематика";
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            isOver = false;
+            string formatDefinition = PreprocessInput(textBox1.Text);
+            if (formatDefinition == "Не указан жанр")
+            {
+                richTextBox1.AppendText("Пользователь: " + textBox1.Text + '\n');
+                richTextBox1.AppendText("Система: Пожалуйста, укажите жанр книги при формировании запроса!" + '\n');
+                return;
+            }
+            string userMessage = textBox1.Text;
+            richTextBox1.Text += "Пользователь: " + userMessage + '\n';
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+                var requestBodyObject = new
+                {
+                    model = "gpt-3.5-turbo",
+                    messages = new object[]
+                    {
+                        new { role = "system", content = systemMessages },
+                        new { role = "user", content = userMessage }
+                    }
+                };
+                Thread loopThreadFunc = new Thread(Looping);
+                loopThreadFunc.Start();
+                string requestBody = JsonConvert.SerializeObject(requestBodyObject);
+                var response = await client.PostAsync(apiUrl, new StringContent(requestBody, Encoding.UTF8, "application/json"));
+                isOver = true;
+                richTextBox1.Text += '\n';
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    JObject jsonResponse = JObject.Parse(responseBody);
+                    JArray choices = (JArray)jsonResponse["choices"];
+                    string message = (string)choices[0]["message"]["content"]; 
+                    richTextBox1.Text += "GPT-3.5: " + message + '\n';
+                    systemMessages += " " + message + " ";
+                }
+                else
+                {
+                    richTextBox1.Text += "Произошла ошибка при отправке запрса!";
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
